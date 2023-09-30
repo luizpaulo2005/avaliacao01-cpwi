cb3c5bf934124b01296276de07237e97f43618cc
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-09-29 21:25:53 -0300

    ex01 concluido

--- ex01/script.js
+++ ex01/script.js
@@ -2,3 +2,17 @@ const input_message = document.querySelector('#input_message')
 const button_message = document.querySelector('#button_message')
 const div_message = document.querySelector('#message-box')
 
+div_message.style.display = "none"
+
+const showMessage = () => {
+    if (input_message.value == "") {
+        alert('A caixa de texto está vazia')
+        div_message.textContent = ""
+    } else {
+        div_message.style.display = "contents"
+        div_message.textContent = input_message.value
+        input_message.value = ""
+    }
+}
+
+button_message.addEventListener('click', showMessage)
