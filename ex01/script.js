277becbcef1516de8e873eb864ce1a4142543ae9
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-09-29 21:41:52 -0300

    prettier + correcoes de css

--- ex01/script.js
+++ ex01/script.js
@@ -1,18 +1,18 @@
-const input_message = document.querySelector('#input_message')
-const button_message = document.querySelector('#button_message')
-const div_message = document.querySelector('#message-box')
+const input_message = document.querySelector("#input_message");
+const button_message = document.querySelector("#button_message");
+const div_message = document.querySelector("#message-box");
 
-div_message.style.display = "none"
+div_message.style.display = "none";
 
 const showMessage = () => {
-    if (input_message.value == "") {
-        alert('A caixa de texto está vazia')
-        div_message.textContent = ""
-    } else {
-        div_message.style.display = "contents"
-        div_message.textContent = input_message.value
-        input_message.value = ""
-    }
-}
+  if (input_message.value == "") {
+    alert("A caixa de texto está vazia");
+    div_message.textContent = "";
+  } else {
+    div_message.style.display = "flex";
+    div_message.textContent = input_message.value;
+    input_message.value = "";
+  }
+};
 
-button_message.addEventListener('click', showMessage)
+button_message.addEventListener("click", showMessage);
