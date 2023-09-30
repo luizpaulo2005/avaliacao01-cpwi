ab0c969f38fe18f208ef9355874b490387c59db1
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-09-29 21:12:44 -0300

    initial commit

---
+++ ex01/script.js
@@ -0,0 +1,4 @@
+const input_message = document.querySelector('#input_message')
+const button_message = document.querySelector('#button_message')
+const div_message = document.querySelector('#message-box')
+
