265adb8c8057111a1b44ef140af9141cda6f1a10
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-09-29 22:09:12 -0300

    ex02 finalizado

---
+++ ex02/script.js
@@ -0,0 +1,24 @@
+const message_box = document.querySelector("#message")
+
+const clock = () => {
+    const agora = new Date();
+    const horas = agora.getHours();
+    const minutos = agora.getMinutes();
+    const segundos = agora.getSeconds();
+
+    if ((horas === 19) && (minutos === 30)) {
+        message_box.textContent = "Vamos para aula"
+    } else if ((horas === 22) && (minutos === 7)) {
+        message_box.textContent = "Hora do Intervalo"
+    } else if ((horas === 22) && (minutos === 8)) {
+        message_box.textContent = "Vamos embora, a aula acabou"
+    } else {
+        message_box.textContent = ""
+    }
+
+    const horaFormatada = `${horas}:${minutos ? `0${minutos}` : minutos}:${segundos < 10 ? `0${segundos}` : segundos}`;
+    const relogio = document.getElementById("relogio");
+    relogio.textContent = horaFormatada;
+  };
+
+setInterval(clock, 1000);
